27410471da41eab4ffc86784502a62d9050c2a8a
Casey Karns <CaseyKarns@gmail.com>
2018-11-07 11:01:20 -0500

    first part

---
+++ Book.rb
@@ -0,0 +1,41 @@
+print "What is your name?
+>"
+name = gets.chomp
+
+puts "Hello #{name}, Pleasure to meet you!"
+
+print "Are you ready for an Adventure?
+>"
+answer = gets.chomp.capitalize
+
+if answer == "Yes"
+  print "Let's Get Started!
+  "
+  @proceed = true
+elsif answer == "No"
+  print "Okay, We can go another day!"
+  @proceed = false
+else
+  print "Please answer Yes or No."
+end
+
+if @proceed == true
+  print "This is going to be a great adventure full of twists and turns and only you decide where we can go! "
+end
+
+if @proceed == false
+print "Goodbye!"
+end
+
+print "You start in a dark forest at a fork in the road. There are two paths you can take, The one on the left which is well lit, but known for treacherous terrain, and angry and hungry animals, or the one on the right which is dark and unknown. Do you choose Left or Right?  "
+answer = gets.chomp.capitalize
+
+if answer == "Left"
+  print "Here we go!"
+  @proceed = true
+elsif answer == "Right"
+  print "If you insist! Let's get to stepping!"
+  @proceed = false
+else
+  print "Please choose a path"
+end
